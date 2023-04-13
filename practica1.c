fe3302e064d846bef246bf7ebea4d8a0a8ec9a00
AlejandroRuzafa <60314401+AlejandroRuzafa@users.noreply.github.com>
2023-04-13 10:37:53 +0200

    Create practica1.c

---
+++ practica1.c
@@ -0,0 +1,10 @@
+// en este programa vamos a comparae dos numeros y coger el mayor de ellos 
+
+int comparar(int number1,int number2) {
+if (number1 > number2) {
+return number1
+}
+else {
+return number2
+}
+}
