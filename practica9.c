d9435dcf72e8ab1b3200978bf490e49770b74c48
AlejandroRuzafa <60314401+AlejandroRuzafa@users.noreply.github.com>
2023-05-13 18:46:21 +0200

    Create practica9.c

---
+++ practica9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include conio.h
+
+main(){
+    char nombre[11];
+    int i=0 ; 
+    printf("introduce el nombre :");
+    do{
+        nombre[i]=getche();
+        i++;
+    }
+    while (i < 9 && nombre[i-1]!='\r');
+    if  (nombre[i-1])=='\r')
+        (nombre[i-1])='\0');
+    else 
+        nombre[i]='\0'
+    printf("\n El nombre es: %s" , nombre);
+}
+
+// Lo he copiado directamente del libro y no compila 
